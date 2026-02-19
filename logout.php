9d352bd54c11016e9f46cffd3bb06f1b90b0d21a
easin07 <113379866+easin07@users.noreply.github.com>
2026-02-19 07:09:24 +0300

    Change logout redirectio
    Updated logout redirection to use loader.php with page parameter.

--- logout.php
+++ logout.php
@@ -1,11 +1,6 @@
 <?php
-
+// logout.php - This file is on GitHub
 require_once('/home/gmpsvasy/public_html/config.php');
-
-// Start session if not already started
-if (session_status() === PHP_SESSION_NONE) {
-    session_start();
-}
 
 // Destroy the session
 session_destroy();
@@ -18,7 +13,7 @@ if (isset($_COOKIE[session_name()])) {
     setcookie(session_name(), '', time() - 3600, '/');
 }
 
-// Redirect to login page (index.php)
-header('Location: index.php');
+// Redirect to login page using the loader
+header('Location: loader.php?page=login');
 exit;
 ?>
